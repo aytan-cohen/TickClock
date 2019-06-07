0bc37b8b20c09fd8aeb35371dc171ce3ccf35120
Jewdeer21 <ayecohen21@gmail.com>
2019-06-07 19:50:22 -0400

    secondary

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = 
+    {
+      time: new Date()
+    }
+  }
+  currentTime()
+  {
+    this.setState({
+      time: new Date()
+    })
+  }
+  componentWillMount()
+  {
+    setInterval(() => this.currentTime, 1000);
+  }
+  render(){
+    return (
+    <h1>
+      The time is: {this.state.time.toLocaleTimeString()}
+    </h1>
+    )
+  }
 }
 
 export default App;
